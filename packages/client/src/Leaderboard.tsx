6f937f15ba189ba5cee692d14ad65e0a596c09d7
Fraser Scott <fraserdscott@gmail.com>
2023-11-13 23:25:06 +0300

    display match rankings

--- packages/client/src/Leaderboard.tsx
+++ packages/client/src/Leaderboard.tsx
@@ -1,3 +1,26 @@
+import { useMUD } from "./MUDContext";
+import { Place } from "./Place";
+
 export function Leaderboard() {
-  return <div>TODO!</div>;
+  const {
+    network: { tables, useStore },
+  } = useMUD();
+
+  const rankings = useStore((state) => state.getRecords(tables.MatchRanking));
+
+  return (
+    <div className="m-1">
+      <div className="text-2xl">Leaderboard</div>
+      {Object.values(rankings).map(({ key, value }) => (
+        <div key={key.key} className="m-1 p-2 border-2">
+          <div className="text-xl">Match #{key.key.slice(2, 10)}:</div>
+          {value.value.map((entity, i) => (
+            <div key={`${key}_${i}`}>
+              <Place index={i} />: {entity}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
