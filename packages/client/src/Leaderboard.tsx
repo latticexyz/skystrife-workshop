3d605911943109c3e377a1eb167291a38fc2dc07
Fraser Scott <fraserdscott@gmail.com>
2023-11-14 00:11:29 +0300

    player names

--- packages/client/src/Leaderboard.tsx
+++ packages/client/src/Leaderboard.tsx
@@ -1,22 +1,44 @@
 import { useMUD } from "./MUDContext";
 import { Place } from "./Place";
+import { Player } from "./Player";
 
 export function Leaderboard() {
   const {
     network: { tables, useStore },
   } = useMUD();
 
-  const rankings = useStore((state) => state.getRecords(tables.MatchRanking));
+  const rankings = useStore((state) => {
+    return Object.values(state.getRecords(tables.MatchRanking)).map(
+      ({ key, value }) => {
+        const matchEntity = key.key;
+        const players = value.value.map((entity) => {
+          const owner = state.getValue(tables.OwnedBy, {
+            matchEntity: key.key,
+            entity,
+          });
+
+          if (owner) return owner.value;
+
+          return entity;
+        });
+
+        return {
+          matchEntity,
+          players,
+        };
+      }
+    );
+  });
 
   return (
     <div className="m-1">
       <div className="text-2xl">Leaderboard</div>
-      {Object.values(rankings).map(({ key, value }) => (
-        <div key={key.key} className="m-1 p-2 border-2">
-          <div className="text-xl">Match #{key.key.slice(2, 10)}:</div>
-          {value.value.map((entity, i) => (
-            <div key={`${key}_${i}`}>
-              <Place index={i} />: {entity}
+      {rankings.map(({ matchEntity, players }) => (
+        <div key={matchEntity} className="m-1 p-2 border-2">
+          <div className="text-xl">Match #{matchEntity.slice(2, 10)}:</div>
+          {players.map((address, i) => (
+            <div key={`${matchEntity}_${address}`}>
+              <Place index={i} />: <Player entity={address} />
             </div>
           ))}
         </div>
--- packages/client/src/Player.tsx
+++ packages/client/src/Player.tsx
@@ -1,3 +1,30 @@
-export function Player() {
-  return <div>{/* TODO */}</div>;
+import { toEthAddress } from "@latticexyz/utils";
+import { Hex } from "viem";
+import { useMUD } from "./MUDContext";
+import { useState } from "react";
+
+export function Player({ entity }: { entity: Hex }) {
+  const {
+    network: { tables, useStore },
+  } = useMUD();
+
+  const [hover, setHover] = useState(false);
+
+  const name = useStore((state) =>
+    state.getValue(tables.Name, { key: entity })
+  );
+
+  return (
+    <span
+      className="w-full"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      {hover ? (
+        <span>{toEthAddress(entity)}</span>
+      ) : (
+        <span>{name ? name.value : toEthAddress(entity)}</span>
+      )}
+    </span>
+  );
 }
